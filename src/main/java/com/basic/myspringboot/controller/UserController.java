ee187c47c163b74c0c96331496e9194bd1122e6e
19013na <19013@naver.com>
2025-05-09 13:18:32 +0900

    feat : Thymeleaf 작업

--- src/main/java/com/basic/myspringboot/controller/UserController.java
+++ src/main/java/com/basic/myspringboot/controller/UserController.java
@@ -2,14 +2,13 @@ package com.basic.myspringboot.controller;
 
 
 import com.basic.myspringboot.entity.User;
-import com.basic.myspringboot.exception.BusinessException;
 import com.basic.myspringboot.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -34,6 +33,41 @@ public class UserController {
         return "index";
     }
 
+    @GetMapping("/signup")
+    public String showSignUpForm(@ModelAttribute("userForm") User user) {
+        return "add-user";
+    }
 
+    @PostMapping("/adduser")
+    public String addUser(@Valid @ModelAttribute("userForm") User user,
+                          Errors result, Model model) {
+        if (result.hasErrors()) {
+            return "add-user";
+        }
+        userRepository.save(user);
+        model.addAttribute("users", userRepository.findAll());
+        return "index";
+        //return "redirect:/index";
+    }
 
+    // user 수정
+    @GetMapping("/edit/{id}")
+    public String showUpdateForm(@PathVariable("id") long id, Model model) {
+        User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+        model.addAttribute("user", user);
+        return "update-user";
+    }
+
+    // BindingResult는 @Valid 실행 결과를 담음.
+    @PostMapping("/update/{id}")
+    public String updateUser(@PathVariable("id") long id,
+                             @Valid @ModelAttribute("user") User user,
+                             BindingResult result) {
+        if (result.hasErrors()) {
+            user.setId(id);
+            return "update-user";
+        }
+        userRepository.save(user);
+        return "redirect:/index";
+    }
 }
--- src/main/java/com/basic/myspringboot/entity/User.java
+++ src/main/java/com/basic/myspringboot/entity/User.java
@@ -1,6 +1,9 @@
 package com.basic.myspringboot.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
@@ -18,9 +21,12 @@ public class User {
     private Long id;
 
     @Column(nullable= false)
+    @NotBlank(message = "Name is mandatory")
     private String name;
 
     @Column(unique = true, nullable= false)
+    @NotBlank(message = "Email is mandatory")
+    @Email  // email 형식 체크
     private String email;
     
     @Column(nullable= false, updatable = false)
